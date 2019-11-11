4e21e962969c8c77a395bc4a530191ca3c9b0315
xuren <xuren2012@126.com>
2019-11-11 17:05:08 +0800

    hello world

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+public class CellApplication {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        SpringApplication.run(CellApplication.class, args);
+        System.out.println("2");
+        SpringApplication.run(CellApplication.class, args);
+		System.out.println("1");
+        SpringApplication.run(CellApplication.class, args);
+        System.out.println("2");
+        SpringApplication.run(CellApplication.class, args);
+    }
+
+}
